44046a7a0ce1056915b247189707407c9c5c9542
Sendy Maulana <sendymaulana54@gmail.com>
2022-01-26 13:54:13 +0700

    Product

---
+++ lib/models/product_model.dart
@@ -0,0 +1,80 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String name;
+  final String category;
+  final String imageUrl;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
+
+  const Product({
+    required this.name,
+    required this.category,
+    required this.imageUrl,
+    required this.price,
+    required this.isRecommended,
+    required this.isPopular,
+  });
+
+  @override
+  List<Object?> get props =>
+      [name, category, imageUrl, price, isRecommended, isPopular];
+
+  static List<Product> products = [
+    Product(
+      name: 'Soft Drink #1',
+      category: 'Soft Drink ',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 10.000,
+      isRecommended: true,
+      isPopular: true,
+    ),
+    Product(
+      name: 'Smoothies #1',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+      price: 12.000,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    Product(
+      name: 'Water #1',
+      category: 'Water',
+      imageUrl:
+          'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+      price: 5.000,
+      isRecommended: false,
+      isPopular: false,
+    ),
+    Product(
+      name: 'Soft Drink #2',
+      category: 'Soft Drink ',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 10.000,
+      isRecommended: true,
+      isPopular: true,
+    ),
+    Product(
+      name: 'Smoothies #2',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+      price: 12.000,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    Product(
+      name: 'Water #2',
+      category: 'Water',
+      imageUrl:
+          'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+      price: 5.000,
+      isRecommended: false,
+      isPopular: false,
+    ),
+  ];
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -20,18 +20,69 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(title: 'E-Commerce'),
       bottomNavigationBar: CustomNavBar(),
-      body: Container(
-          child: CarouselSlider(
-        options: CarouselOptions(
-          aspectRatio: 1.5,
-          viewportFraction: 0.9,
-          enlargeCenterPage: true,
-          enableInfiniteScroll: false,
-        ),
-        items: Category.categories
-            .map((category) => HeroCarouselCard(category: category))
-            .toList(),
-      )),
+      body: Column(
+        children: [
+          Container(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                aspectRatio: 1.5,
+                viewportFraction: 0.9,
+                enlargeCenterPage: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
+              ),
+              items: Category.categories
+                  .map((category) => HeroCarouselCard(category: category))
+                  .toList(),
+            ),
+          ),
+          SectionTitle(title: 'RECOMMENDED'),
+          // Product Card
+          Stack(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 2.5,
+                height: 150,
+                child: Image.network(
+                  Product.products[0].imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                top: 60,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2.5,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withAlpha(50),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 65,
+                left: 5,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2.5 - 10,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        Product.products[0].name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
